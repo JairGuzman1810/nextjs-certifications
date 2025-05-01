ba76b63f2ff4ad2e3ba7a347c7e6445f43d80ffd
JairGuzman1810 <jair18101@hotmail.com>
2025-04-30 18:22:46 -0600

    App Router - Chapter 15

--- app-router/nextjs-dashboard/app/lib/actions.ts
+++ app-router/nextjs-dashboard/app/lib/actions.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
@@ -57,7 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
-  } catch (error) {
+  } catch {
     // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
@@ -96,7 +99,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  } catch (error) {
+  } catch {
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
@@ -107,4 +110,24 @@ export async function updateInvoice(
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      const authError = error as AuthError & { type?: string };
+      switch (authError.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
